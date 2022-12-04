2cb313b7a67961ae5d2e78686f465bad69c574a7
Joebim <josey359@gmail.com>
2022-12-05 00:47:58 +0100

    [ADD] New To do list UI

--- App.js
+++ App.js
@@ -1,62 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React , {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
- 
+import React, {useState} from 'react';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import TaskInputField from './components/TaskInputField';
+import TaskItem from './components/TaskItem';
+
 export default function App() {
- 
-  const [newTask, setnewTask] = useState('');
- 
-  const [appTasks, appTask] = useState([]);
- 
-  const taskInputHandler = (enteredText) => {
-    setnewTask(enteredText);
-  };
- 
-  const addTaskHandler = () =>{
-    appTask(currentTask => [...currentTask, newTask]);
-    console.log(newTask);
-  };
- 
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  }
+
   return (
-    <View style= {styles.container}>
-       
-      <View style = {styles.inputContainer}>
-       
-        <TextInput
-          placeholder = "Task List"
-          style = {styles.input}
-          onChangeText = {taskInputHandler}
-          value = {newTask}
-        />
-       
-        <Button title = "+"
-          onPress = {addTaskHandler}
-        /> 
-      </View>
-      <View>
-        {appTasks.map((task,j) => <Text key={j}>{task}</Text>)}
-      </View>
+    <View style={styles.container}>
+        <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+        tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1E1A3C',
   },
-  input :{
-    borderColor:"black", 
-    borderWidth:1 , 
-    padding :20,
-     
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
   },
-  inputContainer :{
-    flexDirection :'row', 
-    justifyContent :'space-between', 
-    alignContent:'center',
-    bottom:20
+  scrollView: {
+    marginBottom: 70,
   },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
