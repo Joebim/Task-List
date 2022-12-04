6023e5f144965eb559c25076b48bfc4942c86bc1
Joebim <josey359@gmail.com>
2022-12-05 00:30:12 +0100

    [ADD] First commit

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React , {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+ 
 export default function App() {
+ 
+  const [newTask, setnewTask] = useState('');
+ 
+  const [appTasks, appTask] = useState([]);
+ 
+  const taskInputHandler = (enteredText) => {
+    setnewTask(enteredText);
+  };
+ 
+  const addTaskHandler = () =>{
+    appTask(currentTask => [...currentTask, newTask]);
+    console.log(newTask);
+  };
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style= {styles.container}>
+       
+      <View style = {styles.inputContainer}>
+       
+        <TextInput
+          placeholder = "Task List"
+          style = {styles.input}
+          onChangeText = {taskInputHandler}
+          value = {newTask}
+        />
+       
+        <Button title = "+"
+          onPress = {addTaskHandler}
+        /> 
+      </View>
+      <View>
+        {appTasks.map((task,j) => <Text key={j}>{task}</Text>)}
+      </View>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input :{
+    borderColor:"black", 
+    borderWidth:1 , 
+    padding :20,
+     
+  },
+  inputContainer :{
+    flexDirection :'row', 
+    justifyContent :'space-between', 
+    alignContent:'center',
+    bottom:20
   },
 });
